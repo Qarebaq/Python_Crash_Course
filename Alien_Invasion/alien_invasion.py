53f3a44cf13ff17ebf9a4b8b2c81d96bc79ebc4b
Amir Mohammad Mohammadi <amirmohammad.gharebagh1385@gmail.com>
2026-08-29 00:37:26 +0330

    fix

--- Alien_Invasion/alien_invasion.py
+++ Alien_Invasion/alien_invasion.py
@@ -6,15 +6,11 @@ class AlienInvasion:
     def __init__(self):
         pygame.init()
         self.clock = pygame.time.Clock()
-        # self.screen = pygame.display.set_mode((1200,800))
-        pygame.display.set_caption("Alien Invasion")
-        self.ship = Ship(self)
-        # self.bg_color = (230,230,230)
         self.settings = Settings()
-
         self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
+        pygame.display.set_caption("Alien Invasion")
+        self.ship = Ship(self)        
         
-
     def run_game(self):
         while True:
             for event in pygame.event.get():
