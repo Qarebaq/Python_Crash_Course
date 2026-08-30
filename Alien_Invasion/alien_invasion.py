a8ff8ce332e81a722a6fe1402202de65cd703311
Amir Mohammad Mohammadi <amirmohammad.gharebagh1385@gmail.com>
2026-08-31 00:24:37 +0330

    make ship shoot bullet

--- Alien_Invasion/alien_invasion.py
+++ Alien_Invasion/alien_invasion.py
@@ -2,6 +2,7 @@ import sys
 import pygame
 from settings import Settings
 from ship import Ship
+from bullet import Bullet
 class AlienInvasion:
     def __init__(self):
         pygame.init()
@@ -12,12 +13,14 @@ class AlienInvasion:
         self.settings.screen_height = self.screen.get_rect().height
         pygame.display.set_caption("Alien Invasion")
         self.ship = Ship(self)        
+        self.bullets = pygame.sprite.Group()
         
     def run_game(self):
         while True:
             self._check_events()
             # self.screen.fill(self.bg_color)
             self.ship.update()
+            self.bullets.update()
             self._update_screen()
             self.clock.tick(60)
     def _check_events(self):
@@ -35,13 +38,20 @@ class AlienInvasion:
                 self.ship.moving_left = True
             elif event.key == pygame.K_q:
                 sys.exit()
+            elif event.key == pygame.K_SPACE:
+                self._fire_bullet()
     def _check_keyup_events(self,event):
         if event.key == pygame.K_RIGHT:
             self.ship.moving_right = False
         elif event.key == pygame.K_LEFT:
             self.ship.moving_left = False
+    def _fire_bullet(self):
+        new_bullet = Bullet(self)
+        self.bullets.add(new_bullet)
     def _update_screen(self):
             self.screen.fill(self.settings.bg_color)
+            for bullet in self.bullets.sprites():
+                bullet.draw_bullet()
             self.ship.blitme()
 
             pygame.display.flip()
