50e55fae78ccf3c4b46a45b4a19b33c040d95afd
Amir Mohammad Mohammadi <amirmohammad.gharebagh1385@gmail.com>
2026-08-30 21:47:20 +0330

    moving to right feature

--- Alien_Invasion/alien_invasion.py
+++ Alien_Invasion/alien_invasion.py
@@ -15,12 +15,19 @@ class AlienInvasion:
         while True:
             self._check_events()
             # self.screen.fill(self.bg_color)
+            self.ship.update()
             self._update_screen()
             self.clock.tick(60)
     def _check_events(self):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_RIGHT:
+                    self.ship.moving_right = True
+                elif event.type ==pygame.KEYUP:
+                    if event.key == pygame.K_RIGHT:
+                        self.ship.moving_right = False
     def _update_screen(self):
             self.screen.fill(self.settings.bg_color)
             self.ship.blitme()
