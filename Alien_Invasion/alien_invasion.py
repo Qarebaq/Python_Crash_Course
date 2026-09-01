9d873b240ac0a9bf1d368cbbd006294567da37e6
Amir Mohammad Mohammadi <amirmohammad.gharebagh1385@gmail.com>
2026-09-02 00:25:54 +0330

    adding alien to the screen and creating _create_fleet()

--- Alien_Invasion/alien_invasion.py
+++ Alien_Invasion/alien_invasion.py
@@ -3,6 +3,7 @@ import pygame
 from settings import Settings
 from ship import Ship
 from bullet import Bullet
+from alien import Alien
 class AlienInvasion:
     def __init__(self):
         pygame.init()
@@ -14,6 +15,8 @@ class AlienInvasion:
         pygame.display.set_caption("Alien Invasion")
         self.ship = Ship(self)        
         self.bullets = pygame.sprite.Group()
+        self.aliens = pygame.sprite.Group()
+        self._create_fleet()
         
     def run_game(self):
         while True:
@@ -54,11 +57,15 @@ class AlienInvasion:
             for bullet in self.bullets.copy():
                 if bullet.rect.bottom <=0:
                     self.bullets.remove(bullet)
+    def _create_fleet(self):
+        alien = Alien(self)
+        self.aliens.add(alien)
     def _update_screen(self):
             self.screen.fill(self.settings.bg_color)
             for bullet in self.bullets.sprites():
                 bullet.draw_bullet()
             self.ship.blitme()
+            self.aliens.draw(self.screen)
 
             pygame.display.flip()
 
